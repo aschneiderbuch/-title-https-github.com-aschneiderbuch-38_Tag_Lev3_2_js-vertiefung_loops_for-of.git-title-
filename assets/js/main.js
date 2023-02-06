c6638b02c078ff50afca5acfb18bdb3d25967f19
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-06 14:06:23 +0100

    htmlGerüst

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
 console.log("test");
 
+/* 
+Frage
+
+HTMl 
+    2x Input Eingabefelde    Text + CodeSchlüssel
+        Button ?
+    1x Output Ausgabefeld    Verschlüsselter Code 
+
+js
+    let AbcArray = [A,B,C, ... Z]
+    let codeSchlüssel = Number(kommt vom Input CodeSchlüssel);
+
+    Array muss sich weite nach rechts verschieben, z.B. CodeSchlüssel= 1 -> dann A = B  Z = A 
+
+    
+*/
+
+
+// Variablen
+let = iText = document.querySelector("#inputText");
+let = iCode = document.querySelector("#inputCodeSchlüssel");
+let = btn = document.querySelector("#button");
+
+let = output = document.querySelector("#output");
+
+// Eventlistener
+btn = addEventListener("click", () => {});
+
